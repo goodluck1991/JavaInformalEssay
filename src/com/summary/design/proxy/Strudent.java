09fdd3fea3d7d91c91b76edc9248093ecf5bafea
jiaoguojun <vip_jiaoguojun@163.com>
2017-10-16 15:34:52 +0800

    java 代理模式

---
+++ src/com/summary/design/proxy/Strudent.java
@@ -0,0 +1,16 @@
+package com.summary.design.proxy;
+
+/**
+ * @Author: JiaoGuojun
+ * @Description:
+ * @Date: Created in 15:20 2017/10/16
+ */
+public class Strudent {
+    public void eat(){
+        System.out.println("-----person neet eat----");
+    }
+
+    public void learn(){
+        System.out.println("-----学习----");
+    }
+}
